fea1d3f42ae90ddf79128c8fa773f7ce1d440546
David Koch <dave-koch@web.de>
2022-05-05 19:53:43 +0200

    add jwt authentication

--- shack/urls.py
+++ shack/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from papers import views
 
 router = routers.DefaultRouter()
@@ -26,6 +30,9 @@ router.register(r"authors", views.AuthorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # urls to get a jwt token for authentification
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
