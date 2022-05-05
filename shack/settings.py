53f9365b7e720a46279edd83c28596985c9992ea
David Koch <dave-koch@web.de>
2022-05-05 19:53:21 +0200

    enable cors

--- shack/settings.py
+++ shack/settings.py
@@ -41,17 +41,22 @@ INSTALLED_APPS = [
     "taggit",
     "taggit_serializer",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+# change this in production
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "shack.urls"
 
